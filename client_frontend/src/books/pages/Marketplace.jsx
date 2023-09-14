b3088b739cfc8399bd26e99e269bed0b4823ab19
Christopher <valdebenitochristopher@gmail.com>
2023-09-14 17:08:30 -0300

    cambie estilos

--- client_frontend/src/books/pages/Marketplace.jsx
+++ client_frontend/src/books/pages/Marketplace.jsx
@@ -3,7 +3,7 @@ import { MarketplaceHeader, PostList } from '../components'
 export const Marketplace = () => {
   return (
 
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem', marginTop: '5vh' }}>
 
       <h1 style={{ color: '#fff', textAlign: 'center', borderBottom: '2px solid #fff', paddingBottom: '20px', display: 'inline-block' }}>MARKETPLACE</h1>
 
