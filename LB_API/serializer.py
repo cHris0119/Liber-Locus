a93cdacfa945fca364a05fc8c201e00bbacfac75
arturo <artur.cisterna@gmail.com>
2023-09-25 16:36:32 -0300

    serializador modificados

--- LB_API/serializer.py
+++ LB_API/serializer.py
@@ -39,36 +39,6 @@ class AuctionStateSerializer(serializers.ModelSerializer):
         model = AuctionState
         fields = '__all__'
 
-class AuthGroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AuthGroup
-        fields = '__all__'
-
-class AuthGroupPermissionsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AuthGroupPermissions
-        fields = '__all__'
-
-class AuthPermissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AuthPermission
-        fields = '__all__'
-
-class AuthUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AuthUser
-        fields = '__all__'
-
-class AuthUserGroupsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AuthUserGroups
-        fields = '__all__'
-
-class AuthUserUserPermissionsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AuthUserUserPermissions
-        fields = '__all__'
-
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
@@ -112,26 +82,6 @@ class DirectionSerializer(serializers.ModelSerializer):
 class DiscussionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Discussion
-        fields = '__all__'
-
-class DjangoAdminLogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DjangoAdminLog
-        fields = '__all__'
-
-class DjangoContentTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DjangoContentType
-        fields = '__all__'
-
-class DjangoMigrationsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DjangoMigrations
-        fields = '__all__'
-
-class DjangoSessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DjangoSession
         fields = '__all__'
 
 class ForumSerializer(serializers.ModelSerializer):
--- LB_API/views_login.py
+++ LB_API/views_login.py
@@ -1,5 +1,5 @@
 from .serializer import userSerializer
-from .models import User, UserRole, Role, Direction
+from .models import User, UserRole, Role, Direction, Commune
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,26 +7,21 @@ from datetime import datetime
 
 @api_view(['POST'])
 def registerUser(request):
-    Direction.objects.create(
-        
-    )
-    
+    comuna = Commune.objects.get(id = request['id'])
+    Direction.objects.create()
     User.objects.create(
         first_name=request['first_name'],
         last_name=request['last_name'],
         email=request['email'],
         password=request['password'],
-        created_at=datetime.now()
+        created_at=datetime.now(),
         direction=a,
-        user_photo=a,
-        subscription=
-        
-    )
+        user_photo=a )   
         
         
 @api_view(['GET'])
-def loginUser(request, data):
-    user = User.objects.get(id = data)
+def loginUser(request):
+    user = User.objects.get()
     
     serial = userSerializer(user, many=False)
     return Response(serial.data)
