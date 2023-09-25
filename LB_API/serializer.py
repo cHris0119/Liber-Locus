5113506848b236891eff34df4ff97615dc30d7f5
Bruce <brucetapia9@gmail.com>
2023-09-25 16:30:26 -0300

    Los serializaler faltantes
    Todos los serializer de los modelos son creados

--- LB_API/serializer.py
+++ LB_API/serializer.py
@@ -19,4 +19,202 @@ class userRoleSerializer(serializers.ModelSerializer):
         Model: UserRole
         fields = '__all__'
         
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
 
+class AuctionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Auction
+        fields = '__all__'
+
+class AuctionOfferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuctionOffer
+        fields = '__all__'
+
+class AuctionStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuctionState
+        fields = '__all__'
+
+class AuthGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthGroup
+        fields = '__all__'
+
+class AuthGroupPermissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthGroupPermissions
+        fields = '__all__'
+
+class AuthPermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthPermission
+        fields = '__all__'
+
+class AuthUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUser
+        fields = '__all__'
+
+class AuthUserGroupsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUserGroups
+        fields = '__all__'
+
+class AuthUserUserPermissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUserUserPermissions
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class BookCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookCategory
+        fields = '__all__'
+
+class BookStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookState
+        fields = '__all__'
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = '__all__'
+
+class ChatRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatRoom
+        fields = '__all__'
+
+class CommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = '__all__'
+
+class CommuneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Commune
+        fields = '__all__'
+
+class DirectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Direction
+        fields = '__all__'
+
+class DiscussionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discussion
+        fields = '__all__'
+
+class DjangoAdminLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoAdminLog
+        fields = '__all__'
+
+class DjangoContentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoContentType
+        fields = '__all__'
+
+class DjangoMigrationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoMigrations
+        fields = '__all__'
+
+class DjangoSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoSession
+        fields = '__all__'
+
+class ForumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Forum
+        fields = '__all__'
+
+class ForumCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumCategory
+        fields = '__all__'
+
+class ForumUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumUser
+        fields = '__all__'
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
+
+class PaymentMethodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentMethod
+        fields = '__all__'
+
+class PostVentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostVenta
+        fields = '__all__'
+
+class PurchaseDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseDetail
+        fields = '__all__'
+
+class PurchaseDetailStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseDetailState
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class RefundSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Refund
+        fields = '__all__'
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class ReviewLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewLike
+        fields = '__all__'
+
+class StatePostVentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StatePostVenta
+        fields = '__all__'
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+
+class UserRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRoom
+        fields = '__all__'
