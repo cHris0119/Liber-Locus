5ce35c1f9d5ca440e628e85518637aff5401171f
Bruce <brucetapia9@gmail.com>
2023-09-26 12:42:30 -0300

    Creacion de vistas y url
    para la update y creacion de libros

--- LB_API/views_marketPlace.py
+++ LB_API/views_marketPlace.py
@@ -1,15 +1,10 @@
 from rest_framework import viewsets
-from .serializer import BookSerializer
-from .models import Book  
-from .serializer import BookCategorySerializer  
-from .serializer import CommentsSerializer
-from .models import Comments 
+from .serializer import BookSerializer, BookCategorySerializer, CommentsSerializer
+from .models import Book, Comments, BookCategory
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response  
-from .models import Book  
-from .models import BookCategory
-
+from rest_framework import status
 
 class BookView(viewsets.ModelViewSet):
     serializer_class = BookSerializer
@@ -22,3 +17,22 @@ class BookCategoryView(viewsets.ModelViewSet):
 class CommentsView(viewsets.ModelViewSet):
     serializer_class = CommentsSerializer
     queryset = Comments.objects.all()
+
+@api_view(['POST'])
+def book_create(request):
+    if request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def book_update(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
