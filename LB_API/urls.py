95f4fec9d85cea4e8be1aab5a74348b1f78b687f
arturo <artur.cisterna@gmail.com>
2023-09-26 20:28:01 -0300

    views

--- LB_API/urls.py
+++ LB_API/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path, include, re_path
 from rest_framework import routers
-from LB_API import views as vw
+from LB_API import views_marketPlace as vw
 from LB_API import views_login as vl
 from rest_framework.authtoken import views
 
 
-router = routers.DefaultRouter()
-router.register(r'users', vw.userView, 'users')
-
 urlpatterns = [
-    path("api/", include(router.urls)),
-    path('gettoken/', views.obtain_auth_token),
-    path('login/', vl.loginUser, name='login'),
-    path('registerUser/', vl.registerUser, name='registerUser'),
-    path('api/books/create/', views.book_create, name='book_create'),  # Ruta para agregar un libro (POST)
-    path('api/books/update/<int:pk>/', views.book_update, name='book_update')  # Ruta para actualizar un libro por ID (PUT)
+    path('api/gettoken/', views.obtain_auth_token),
+    path('api/login/', vl.loginUser, name='login'),
+    path('api/registerUser/', vl.registerUser, name='registerUser'),
+    path('api/editarUser/int:id/', vl.editUser, name='editUser'),
+    path('api/books/create/', vw.book_create, name='book_create'),  # Ruta para agregar un libro (POST)
+    path('api/books/update/<int:pk>/', vw.book_update, name='book_update')  # Ruta para actualizar un libro por ID (PUT)
 ]
