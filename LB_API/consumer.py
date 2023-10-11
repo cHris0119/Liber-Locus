45b53aa130e24546f58273012a06994ab7b8f2d8
arturo <artur.cisterna@gmail.com>
2023-10-11 17:38:10 -0300

    likes user

--- LB_API/consumer.py
+++ LB_API/consumer.py
@@ -2,12 +2,13 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
 from .models import ReviewLike, Review,User
+from views_login import int_id
 
 class LikesConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
         
-    def disconnect(self, close_code):
+    def disconnect(self):
         self.close()
     
     def receive(self, text_data):
@@ -21,7 +22,15 @@ class LikesConsumer(WebsocketConsumer):
             review_id = data.get('review_id')
             review = Review.objects.get(id = review_id)
             like_exists = ReviewLike.objects.filter(user=user, review=review).exists()
+            if like_exists:
+                likes = ReviewLike.objects.filter(review=review).count()
             
-            likes = ReviewLike.objects.filter(review=review).count()
-            
-            self.send(text_data=json.dumps({'user_like': like_exists, 'likes': likes}))
+                self.send(text_data=json.dumps({'user_like': like_exists, 'likes': likes}))
+            else:
+                ReviewLike.objects.create(
+                    id = int_id(),
+                    user = user,
+                    review = review
+                )
+                like_exists = ReviewLike.objects.filter(user=user, review=review).exists()
+                self.send(text_data=json.dumps({'user_like': like_exists, 'likes': likes}))
