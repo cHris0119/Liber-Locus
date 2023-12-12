3bf61a529b70217a3eb875f0abb746f6f1b68ffc
arturo <artur.cisterna@gmail.com>
2023-12-11 22:23:41 -0300

    ws areglos

--- LB_API/consumer.py
+++ LB_API/consumer.py
@@ -88,7 +88,7 @@ class AuctionConsumer(AsyncWebsocketConsumer):
             self.auction_group_name,
                 {
                     'type': 'send_price_update',
-                    'message': subasta.final_price,
+                    'message': str(subasta.final_price),
                 }
             )
         
